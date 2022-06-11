ab70322f419894f130b092a5c661f6f7172febd1
stephen <stephengeospy@gmail.com>
2022-06-11 17:48:54 -0400

    Add NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import NavBar from "./NavBar";
+
 function App() {
   return (
     <div>
-      App
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,132 @@
+import { React, useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import useOnClickOutside from "../hooks/useOnClickOutside";
+
+function NavBar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const ref = useRef();
+  useOnClickOutside(ref, () => setIsDropdownOpen(!isDropdownOpen));
+
+  return (
+    <div className="dark-mode:text-gray-200 dark-mode:bg-gray-800 w-full bg-white text-gray-700">
+      <div className="mx-auto flex max-w-screen-xl flex-col px-4 md:flex-row md:items-center md:justify-between md:px-6 lg:px-8">
+        <div className="flex flex-row items-center justify-between p-4">
+          <Link
+            to="#"
+            className="dark-mode:text-white focus:shadow-outline rounded-lg text-lg font-semibold uppercase tracking-widest text-gray-900 focus:outline-none"
+          >
+            National Youth Bike Council
+          </Link>
+          <button
+            className="focus:shadow-outline rounded-lg focus:outline-none md:hidden"
+            onClick={() => setIsNavOpen(!isNavOpen)}
+          >
+            <svg fill="currentColor" viewBox="0 0 20 20" className="h-6 w-6">
+              {!isNavOpen && (
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              )}
+              {isNavOpen && (
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              )}
+            </svg>
+          </button>
+        </div>
+        <nav
+          className={`${
+            isNavOpen ? `flex` : `hidden`
+          } flex-grow flex-col pb-4 md:flex md:flex-row md:justify-end md:pb-0`}
+        >
+          <Link
+            className="dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 focus:shadow-outline mt-2 rounded-lg bg-gray-200 px-4 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0"
+            to="#"
+          >
+            Blog
+          </Link>
+          <Link
+            className="dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 focus:shadow-outline mt-2 rounded-lg bg-transparent px-4 py-2 text-sm font-semibold hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0 md:ml-4"
+            to="#"
+          >
+            Portfolio
+          </Link>
+          <Link
+            className="dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 focus:shadow-outline mt-2 rounded-lg bg-transparent px-4 py-2 text-sm font-semibold hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0 md:ml-4"
+            to="#"
+          >
+            About
+          </Link>
+          <Link
+            className="dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 focus:shadow-outline mt-2 rounded-lg bg-transparent px-4 py-2 text-sm font-semibold hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0 md:ml-4"
+            to="#"
+          >
+            Contact
+          </Link>
+          <div ref={ref} className="relative">
+            <button
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              className="dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 focus:shadow-outline mt-2 flex w-full flex-row items-center rounded-lg bg-transparent px-4 py-2 text-left text-sm font-semibold hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0 md:ml-4 md:inline md:w-auto"
+            >
+              <span>Dropdown</span>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                className={`mt-1 ml-1 inline h-4 w-4 transform transition-transform duration-200 md:-mt-1 ${
+                  isDropdownOpen ? `rotate-180` : `rotate-0`
+                }`}
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            {isDropdownOpen && (
+              <div
+                // transition-enter="transition ease-out duration-100"
+                // transition-enter-start="transform opacity-0 scale-95"
+                // transition-enter-end="transform opacity-100 scale-100"
+                // transition-leave="transition ease-in duration-75"
+                // transition-leave-start="transform opacity-100 scale-100"
+                // transition-leave-end="transform opacity-0 scale-95"
+                className="absolute right-0 mt-2 w-full origin-top-right rounded-md shadow-lg transition md:w-48"
+              >
+                <div className="dark-mode:bg-gray-800 rounded-md bg-white px-2 py-2 shadow">
+                  <Link
+                    className="dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 focus:shadow-outline mt-2 block rounded-lg bg-transparent px-4 py-2 text-sm font-semibold hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0"
+                    to="#"
+                  >
+                    Link #1
+                  </Link>
+                  <Link
+                    className="dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 focus:shadow-outline mt-2 block rounded-lg bg-transparent px-4 py-2 text-sm font-semibold hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0"
+                    to="#"
+                  >
+                    Link #2
+                  </Link>
+                  <Link
+                    className="dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 focus:shadow-outline mt-2 block rounded-lg bg-transparent px-4 py-2 text-sm font-semibold hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0"
+                    to="#"
+                  >
+                    Link #3
+                  </Link>
+                </div>
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
